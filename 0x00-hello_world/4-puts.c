ab07d50a5658f15731dfd1c8c5cb27e362f99520
mayma <cammillusmayma@gmail.com>
2022-06-04 04:31:57 -0700

    task done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 include < stdio.h >
 /**
- * *  *main - entry point
- *  *
- *   *  *return: always 1
- *    **/
+ *main - entry point
+ *return: 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

e9f295a083e017f895dd8ab06a4d6b50977fba12
mayma <cammillusmayma@gmail.com>
2022-06-08 06:29:33 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower -> checks if the character is lowcase
+ * @C: a character argmunet
+ * Return: returns 1 if c is lowacase and 0 if not
+ */
+int _islower(int c)
+{
+	return (C > 'a' C <= 'z');
+}

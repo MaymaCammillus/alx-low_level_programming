bf8efcade3d7edfc1138316857fe383b6fb5c66d
mayma <cammillusmayma@gmail.com>
2022-06-08 02:53:47 -0700

    putchar printed

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - orint putchar
+ * Description : print_putchar
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c")
+	_putchar("h");
+	_putchar("a");
+	_puthchar("r");
+	_putchar("\n");
+
+	return (0);
+}

9f4a8f5ee7d2010ed1d7edde85801c8c9af7ce05
mayma <cammillusmayma@gmail.com>
2022-06-08 09:43:04 -0700

    There are only 3 colors

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - prints 24 hours
+ */
+
+void jack_bauer(void)
+{
+	int i; j;
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			if(i < 10)
+			{
+				_putchar('0');
+				_putchar(i + '0');
+			}
+			if(j < 10)
+			{
+
+			_putchar(i); _putchar(':');
+			_putchar(j);
+

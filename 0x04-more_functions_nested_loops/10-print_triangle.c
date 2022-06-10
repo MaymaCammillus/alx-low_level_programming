e1a098ce75821017b89114c6abe2c4fd01ffc37d
mayma <cammillusmayma@gmail.com>
2022-06-09 18:40:01 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,10 +8,11 @@ void print_triangle(int size)
 {
 int a, b, c;
 if (size <= 0)
+_putchar('\n');
 else
-for (a = 0; a < size; a++)
+for (a = 0; a < size; a--)
 {
-for (b = 0; b < size; b++)
+for (b = 0; b < size; b--)
 {
 for (c = 0; c < size; c++)
 {

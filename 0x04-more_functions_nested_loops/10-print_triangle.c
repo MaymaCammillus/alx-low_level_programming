ab49d3d3363a882127ce31efc44a80dde4468007
mayma <cammillusmayma@gmail.com>
2022-06-09 18:08:13 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - prints a line
+ * @size: character argument
+ */
+
+void print_square(int size)
+{
+int a, b, c;
+
+if (size <= 0)
+_putchar('\n');
+else
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++
+{
+for (c = 0; c < size; c++)
+}
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
+

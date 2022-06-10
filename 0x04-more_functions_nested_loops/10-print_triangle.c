33d0dca611a35431e5dfa9a00e232305b65f33bb
mayma <cammillusmayma@gmail.com>
2022-06-09 19:13:07 -0700

    Numbers have life

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,14 +7,15 @@
 void print_triangle(int size)
 {
 int a, b, c;
+
 if (size <= 0)
 _putchar('\n');
 else
-for (a = 0; a < size; a--)
 {
-for (b = 0; b < size; b--)
+for (a = 0; a < size; a++)
 {
-for (c = 0; c < size; c++)
+for (b = size - 1; b--)
+for (c = a + 1; c--)
 {
 _putchar('#');
 }
@@ -22,3 +23,4 @@ _putchar('\n');
 }
 }
 }
+

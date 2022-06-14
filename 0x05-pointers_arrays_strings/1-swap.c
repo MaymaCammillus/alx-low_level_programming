a67b208e19fbef3306a2342371c4fb4f1831ba54
mayma <cammillusmayma@gmail.com>
2022-06-14 03:39:12 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- *swap_int - swapping the values of two integres
- *@a: parameter1
- *@b: parameter2
+ * swap_int - swapping the values of two integres
+ * @a: parameter1
+ * @b: parameter2
  */
 
 void swap_int(int *a, int *b)
@@ -10,6 +10,6 @@ void swap_int(int *a, int *b)
 	int tmp;
 
 	tmp = *a
-	* a = *b
-	* b = tmp
+	* a = *b;
+	*b = tmp;
 }
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -1,3 +1,1 @@
-[master 51ebb02] do not swap horses in crossing a stream
- 2 files changed, 17 insertions(+)
- create mode 100644 0x05-pointers_arrays_strings/1-swap.c
+ pointer, string and arrays

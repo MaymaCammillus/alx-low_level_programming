d9e7702f4851650b75bc948b25a8cd77d7c4f0c9
mayma <cammillusmayma@gmail.com>
2022-06-15 01:28:24 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncat - a function that concatenates two strings
+ * @dest: Pointer to the destination array where the content is to be copied
+ * @src: The string to cpy from
+ * @n: n
+ * Return: Always 0 (Success)
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+
+	for (j = 0; j < n; j++)
+	{
+		dest[i + j] = src[j];
+		if (src[j] == '\0')
+			j = n;
+	}
+	return (dest);
+}

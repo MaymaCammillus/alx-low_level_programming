a8c58d9ba8b0b502633b20ca276e54b535a3f23c
mayma <cammillusmayma@gmail.com>
2022-06-16 04:07:46 -0700

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,9 +14,9 @@ i = j = 0;
 while (*(dest + i))
 i++;
 
-for (j = 0; j < n && *(src + j) != '\0'; j++)
-*(dest + i) = *(src + j);
+for (j = 0; j < n && (src + j) != '\0'; j++)
+(dest + i) = (src + j);
 if (j < n)
-*(dest + i) = *(src + j);
+(dest + i) = (src + j);
 return (dest);
 }

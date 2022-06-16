f01cde46976bbee55a537bceb1fc734db92dfc9c
mayma <cammillusmayma@gmail.com>
2022-06-16 06:55:22 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,8 +8,8 @@
 char *leet(char *s)
 {
 	int i, j;
-	char r[] = {aAeEoOtTlL};
-	char n[] = {4433007711};
+	char r[] = {'a', 'A', 'e', 'E', 'o' 'O', 't', 'T', 'l', 'L'};
+	char n[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	for (i = 0; s[i] != '\0'; i++)
 	{

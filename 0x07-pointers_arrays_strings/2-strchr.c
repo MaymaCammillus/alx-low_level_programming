b65070024389b2101f9b894e760ae4603162cdaa
mayma <cammillusmayma@gmail.com>
2022-06-20 13:53:34 -0700

    task2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,24 +1,29 @@
 #include "main.h"
 /**
-* _strchr - locate character in string
-* @s: source string
-* @c: character to find
-* Return: the string from character found
-*/
+  * _strchr - locate character in string
+  * @s: source string
+  * @c: character to find
+  *
+  * Return: the string from character found
+  */
 char *_strchr(char *s, char c)
 {
-int i = 0, j;
-while (s[i])
-{
-i++;
+	int a = 0, b;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+
+	return ('\0');
 }
-for (j = 0; j < i; j++)
-{
-{
-if (c == s[j])
-s += j;
-return (s);
-}
-}
-return ('\0');
-}
+

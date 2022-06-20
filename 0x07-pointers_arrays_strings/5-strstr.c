bcee28dd095e62b52fa37b3aebb5b998843a79ee
mayma <cammillusmayma@gmail.com>
2022-06-20 14:11:04 -0700

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+3include "main.h"
+/**
+  * _strstr - locate a substring
+  * @haystack: the string to search
+  * @needle: the string to find
+  * Return: char value
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0, j = 0;
+
+	while (haystack[i])
+	{
+		while (needle[j])
+		{
+			if (haystack[i + j] != needle[i])
+			{
+				break;
+			}
+
+			j++;
+		}
+
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+
+		i++;
+	}
+
+	return ('\0');
+}

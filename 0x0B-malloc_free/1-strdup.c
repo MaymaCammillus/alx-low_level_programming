0a5cbc4f1a73b8f046474f822e81615848db700b
mayma <cammillusmayma@gmail.com>
2022-06-27 07:27:33 -0700

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+*_strdup - Copy a string and return pointer to new string
+*@str: String to be copied
+*Return: Null or Pointer to new string
+*/
+char *_strdup(char *str);

7188d94fcc515d054cb271fc65dc6c30ae12a83e
mayma <cammillusmayma@gmail.com>
2022-06-29 06:27:37 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -13,13 +13,11 @@ int **alloc_grid(int width, int height)
 	int i, j;
 
 	if (width <= 0 || height <= 0)
-	{
 		return (NULL);
-	}
 	grid = malloc(sizeof(int *) * height);
 	if (grid == NULL)
 	{
-		return (NULL);
+		free(grid);
 	}
 	for (i = 0; i < height; i++)
 	{

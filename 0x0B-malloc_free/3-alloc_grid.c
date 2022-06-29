2820fb03812e190d590cdbb0a1883fab54c7f465
mayma <cammillusmayma@gmail.com>
2022-06-29 06:38:39 -0700

    task3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,42 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
-
 /**
- * alloc_grid - allocates a grid, make space and free space
- * @width: takes in width of grid
- * @height: height of grid
- * Return: grid with freed spaces
+ * alloc_grid - function that returns a pointer
+ * to a 2D array of intergers.
+ * @width: width int
+ * @height: height int
+ * Return: int
  */
-
 int **alloc_grid(int width, int height)
 {
-	int **grid;
-	int i, j;
+	int **a, i, j;
 
-	if (width <= 0 || height <= 0)
+	a = malloc(sizeof(*a) * height);
+	if (width <= 0 || height <= 0 || a == 0)
 		return (NULL);
-	grid = malloc(sizeof(int *) * height);
-	if (grid == NULL)
-	{
-		free(grid);
-	}
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = malloc(sizeof(int) * width);
-
-		if (grid[i] == NULL)
+		a[i] = malloc(sizeof(**a) * width);
+		if (a[i] == 0)
 		{
-			for (i = i - 1; i >= 0; i--)
-			{
-				free(grid[i]);
-			}
-
-			free(grid);
+			while (i--)
+			free(a[i]);
+			free(a);
 			return (NULL);
 		}
+		for (j = 0; j < width; j++)
+			a[i][j] = 0;
 	}
-
-	for (i = 0; j < width; j++)
-		grid[i][j] = 0;
-
-	return (grid);
+	return (a);
 }

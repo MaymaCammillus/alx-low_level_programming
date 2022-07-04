b5b9d265d14089beca2b2622bc53bba1274a3bc8
mayma <cammillusmayma@gmail.com>
2022-07-04 09:40:31 -0700

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,6 +12,17 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
-#endif
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+int _putchar(char);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*

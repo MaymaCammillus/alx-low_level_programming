ef0d49ab1af095ce804fc717366cb415d013defa
mayma <cammillusmayma@gmail.com>
2022-07-08 07:09:06 -0700

    valuable name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 /**
  * print_name - prints the name
- * @name: name to be printed
- * @f: function that prints the name
+ * @name: name
+ * @f: function
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL && name != NULL)
-		(*f)(name);
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
 }

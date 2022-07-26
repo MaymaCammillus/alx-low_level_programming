18133efc68af595cf75612492f51102e9d233f84
mayma <cammillusmayma@gmail.com>
2022-07-26 14:33:26 -0700

    101

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,7 +13,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int j, res;
 
 	res = 0;
-	for j = 8 * sizeof(j) - 1; j >= 0; j--)
+	for (j = 8 * sizeof(n) - 1; j >= 0; j--)
 		if (((n ^ m) >> j) & 1)
 			res++;
 	return (res);
